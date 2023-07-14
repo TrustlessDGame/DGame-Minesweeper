01e1fb17ccd552636071a4cc326e20912e590882
sigmar-const <archaon1574@trustless.computer>
2023-07-14 10:39:17 +0700

    redraw board

--- game/5.gameplay.js
+++ game/5.gameplay.js
@@ -12,7 +12,7 @@ function generateBoard(rows, columns, numMines) {
       board[i][j] = {
         isMine: false,
         adjacentMines: 0,
-        isRevealed: false
+        isRevealed: false,
       };
     }
   }
@@ -33,7 +33,13 @@ function generateBoard(rows, columns, numMines) {
   for (const [row, col] of mines) {
     for (let i = row - 1; i <= row + 1; i++) {
       for (let j = col - 1; j <= col + 1; j++) {
-        if (i >= 0 && i < rows && j >= 0 && j < columns && !(i === row && j === col)) {
+        if (
+          i >= 0 &&
+          i < rows &&
+          j >= 0 &&
+          j < columns &&
+          !(i === row && j === col)
+        ) {
           board[i][j].adjacentMines++;
         }
       }
@@ -52,7 +58,7 @@ function revealCell(board, row, col) {
     // If the cell is a mine, the game is over
     if (cell.isMine) {
       // Implement game over logic here
-      console.log('Game Over!');
+      console.log("Game Over!");
       return;
     }
 
@@ -67,6 +73,52 @@ function revealCell(board, row, col) {
       }
     }
   }
+  drawBoard(board);
+}
+
+function drawBoard(newBoard) {
+  // Code to redraw the board
+  const grid = document.getElementById("grid");
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  newBoard.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      const square = document.createElement("div");
+      square.setAttribute("data-row", rowIndex);
+      square.setAttribute("data-col", colIndex);
+      square.setAttribute("class", "valid");
+      square.classList.add("square");
+      grid.appendChild(square);
+      // squares.push(square);
+
+      const cell = newBoard[rowIndex][colIndex];
+      const cellElement = document.querySelector(
+        `[data-row="${rowIndex}"][data-col="${colIndex}"]`
+      );
+
+      if (cell.isRevealed) {
+        if (cell.isMine) {
+          square.classList.add("mine");
+        } else if (cell.adjacentMines > 0) {
+          const classNumber = {
+            1: "one",
+            2: "two",
+            3: "three",
+            4: "four",
+          };
+          square.classList.add(`${classNumber[cell.adjacentMines]}`);
+          square.innerHTML = cell.adjacentMines;
+        } else {
+          square.classList.add("checked");
+        }
+      }
+
+      cellElement.addEventListener("click", function (e) {
+        revealCell(board, rowIndex, colIndex);
+      });
+    });
+  });
 }
 
 // Code
@@ -75,30 +127,28 @@ const columns = 10;
 const numMines = 10;
 const board = generateBoard(rows, columns, numMines);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.getElementById('grid');
-  let squares = [];
+document.addEventListener("DOMContentLoaded", () => {
+  // const grid = document.getElementById("grid");
+  // let squares = [];
+
+  drawBoard(board);
 
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < columns; col++) {
-      const square = document.createElement("div");
-      square.setAttribute("data-row", row);
-      square.setAttribute("data-col", col);
-      // square.setAttribute("class", 'valid');
-      square.classList.add("square");
-      grid.appendChild(square);
-      squares.push(square);
+      const cell = document.querySelector(
+        `[data-row="${row}"][data-col="${col}"]`
+      );
 
       // normal click
-      square.addEventListener("click", function (e) {
+      cell.addEventListener("click", function (e) {
         revealCell(board, row, col);
       });
 
       // cntrl and left click
-      square.oncontextmenu = function (e) {
+      cell.oncontextmenu = function (e) {
         e.preventDefault();
         // addFlag(square);
       };
     }
   }
-})
+});
