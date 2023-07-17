2cfc58ce4828353f279bb73218c34e22ac8bb6c9
genesis-0000 <thaibao@autonomous.nyc>
2023-07-17 11:02:07 +0700

    - update

---
+++ contracts/migrations/game/1.1_deploy.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import {MineSweeper} from "./minesweeper";
+
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "nos_testnet") {
+            console.log("wrong network");
+            return;
+        }
+        const game = new MineSweeper(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const address = await game.deployUpgradeable(
+            process.env.PUBLIC_KEY,
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000000",
+        );
+        console.log("%s MineSweeper address: %s", process.env.NETWORK, address);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
---
+++ contracts/migrations/game/1.2_upgradeContract.ts
@@ -0,0 +1,17 @@
+import {MineSweeper} from "./minesweeper";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "nos_testnet") {
+            console.log("wrong network");
+            return;
+        }
+        const args = process.argv.slice(2);
+        const nft = new MineSweeper(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const address = await nft.upgradeContract(args[0]);
+        console.log({address});
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
